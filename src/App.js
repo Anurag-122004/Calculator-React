7b702fca8dbbc42633bf6aa8228216fa829920ab
Anurag Kaushik <anuragofficial.wrk@gmail.com>
2024-02-15 01:17:34 +0530

    name changed

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
   return ( 
     <div className="App"> 
       <div> 
-        <h1>Simplest Working Calculator</h1> 
+        <h1>Simple Working Calculator</h1> 
       </div> 
       <form> 
         <p ref={resultRef}> 
